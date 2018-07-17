c158c43e69325dbff5b2394f7d16c4f7de745a0a
Josue Lopez <galvez2j@gmail.com>
2018-07-16 23:08:41 -0600

    Update to wiki_summary_extract.py
    I added comments at the top of the file noting the time and version this module was created.
    
    I also modified the short comment that outlines the purpose of the module.
    
    Lastly, I modified the source code. It now includes a "status" variable that helps the program print a success message at the end of execution.

--- wiki_summary_extract.py
+++ wiki_summary_extract.py
@@ -1,4 +1,6 @@
 # Creator Josue Lopez
+# Mon Jul 16 10:30 PM
+# Python 3.6
 
 import urllib.request
 import urllib.parse
@@ -6,10 +8,12 @@ import urllib.error
 import json
 
 """
-This script / module is a basic script that makes use of wikipedia's public API.
-cquires the information of anything that is searched
-using the initial command prompt. This module returns a summary of whatever
-was searched using wikipedia.
+This module is a basic script that searches for a simple summary
+of anything using Wikimedia's REST API. It's a basic interface
+search tool written in Python 3.6.
+
+When the program is executed, it will run the 'main' function
+which handldes the entire module.
 """
 
 """
@@ -18,12 +22,14 @@ I agree to Wikimedia's general Term's of Use and Private policy.
 """
 
 def main():
-    title = input("Search for something on wikipedia: ")
-
+    title = input("Search for a famous name on wikipedia: ")
+    status = None
     try:
+        print('...')
         url = urllib.request.urlopen("https://en.wikipedia.org/api/rest_v1/page/summary/"+title)
+        status = True
     except urllib.error.HTTPError:
-        print('\nSorry. Our wiki_extract module believe this search does not exist.\n')
+        print("\nSorry, an error occurred. Our wiki_extract module believes this search does not exist in Wikimedia's database.\n")
     else:
         response = url.read().decode(encoding ='utf-8')
 
@@ -31,6 +37,9 @@ def main():
 
         print('\n' + json_data['displaytitle'] + '\n')
         print(json_data['extract'].encode('ascii', 'ignore').decode('ascii') + '\n')
+    finally:
+        print("Success.") if status is True else print('Try Again.')
+        print("Done.")
 
 if __name__ == '__main__':
     main()
