b299d2edcb22e2c15ffbc2ff874600432a718f62
Josue Lopez <galvez2j@gmail.com>
2020-10-23 02:30:49 -0700

    refactor(lint.sh): beautify bash script

--- src/scripts/lint.sh
+++ src/scripts/lint.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 # Linter are static code analysis tools that flags syntax errors, bugs, style
 
-# Get list of all files that were Git Added (passed down for github)
+printf "lint.sh: Starting..."
+
+# if this script called and given any commandline args 
 if [[ $# > 1 ]];then
+	# Extract array arg passed down via commandline
 	ARRAY=( "$@" )
 	last_idx=$(( ${#ARRAY[@]} - 1 ))
 	unset array[$last_idx]
-
-	echo "LINT.SH array contains:"
-	printf "%s\n" "${ARRAY[@]}"
 else
 	current_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p');
+
+	# get list of files that were 'git add' or 'git commit' and don't exist on master
 	ARRAY=($(git diff origin/master $current_branch --name-only HEAD));
-	printf "\nFollowing files are differnet between 'origin/master' and '${current_branch}':\n\n"
-	printf "%s\n" "${ARRAY[@]}"
 fi
+
+printf "\nFollowing files are differnet between 'origin/master' and '${current_branch}':\n\n"
+printf "%s\n" "${ARRAY[@]}"
 
 
 # Get the number of Python files currently Git Added
